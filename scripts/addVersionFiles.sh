7595ec9bab037a673bd48616511513708e6792dc
James Carmichael <james.carmichael@hcl.com>
2024-06-19 16:03:53 -0400

    Update addVersionFiles.sh

--- scripts/addVersionFiles.sh
+++ scripts/addVersionFiles.sh
@@ -5,40 +5,33 @@
 #   - udclient weburl (DS_WEB_URL) is set
 #   - JAVA_HOME environment variable is set
 
-usage() {
-    echo "usage: $0 cmdpath componentName componentVersion base offset include exclude saveExecuteBits"
-    echo "cmdpath is the fully-qualified pathname for the udclient executable (required)"
-    echo "componentName is the name/id of the component (required)"
-    echo "componentVersion is the name/id of the component version (required)"
-    echo "base if the local base directory for upload.  All files inside this will be sent. (required)"
-    echo "offset is the target path (the directory in the version files to which these files should be added. (optional)"
-    echo "include is an include file pattern for selecting files to add (may be repeated). (optional)"
-    echo "exclude is an exclude file pattern for excluding files (may be repeated). (optional)"
-    echo "saveExecuteBits specifies whether or not to save execute bits for files. (optional)"
-    exit 1
-}
-
-# Must specify cmdpath, componentName, componentVersion and base
-if [ $# -lt 4 ]
-then
-    usage
-fi
-
-set -x
-
-# Make sure that the cmdpath specifies and executable file
-if [ -x "$1" ]
-then
-    CMD_PATH=$1
-else
-    echo "command path $1 is not an executable"
-    usage
-fi
-
 # Create the command to execute
-BASE_CMD="$CMD_PATH addVersionFiles -component '$2' -version '$3 -base '$4'"
+if [ -z FILES_CMD ]
+  echo "udclient command not specified.  Exiting"
+  exit 1
+else
+  BASE_CMD="\"$CMD_PATH\" addVersionFiles"
 
-if [ -z $5 ]
+# Specify component name
+if [ -z $FILES_COMPONENTNAME ]
+  echo "Component name not specified.  Exiting"
+  exit 1
+else
+  BASE_CMD = "$BASE_CMD -component \"$FILES_COMPONENTNAME\""
+
+# Specify component version name
+if [ -z $FILES_VERSIONNAME ]
+  echo "Version name not specified.  Exiting"
+  exit 1
+else
+  BASE_CMD = "$BASE_CMD -version \"$FILES_VERSIONNAME\""
+
+if [ -z $FILES_BASE ]
+  echo "No base files specified. Exiting"
+  exit 1
+else
+  BASE_CMD = "$BASE_CMD -base \"$FILES_BASE\""
+fi
 
 # Invoke the udclient to add version files to the component version
-$BASE_CMD
+eval $BASE_CMD
