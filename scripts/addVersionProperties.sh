cd15685959594bcd36c7bff621dfef4ac7f81ee6
James Carmichael <james.carmichael@hcl.com>
2024-06-20 15:27:04 -0400

    Update addVersionProperties.sh

--- scripts/addVersionProperties.sh
+++ scripts/addVersionProperties.sh
@@ -17,61 +17,32 @@ else
   base_cmd="\"$FILES_CMD\" setVersionProperty"
 fi
 
-# Create the command to execute
+# Specify component name
+if [ -z $VERSION_PROPERTIES_COMPONENTNAME ]
+then
+  echo "Component name not specified.  Exiting"
+  exit 1
+else
+  base_cmd+=" -component \"$VERSION_PROPERTIES_COMPONENTNAME\""
+fi
+
+# Specify component version name
+if [ -z "$VERSION_PROPERTIES_VERSIONNAME" ]
+then
+  echo "Version name not specified.  Exiting"
+  exit 1
+else
+  base_cmd+=" -version \"$VERSION_PROPERTIES_VERSIONNAME\""
+fi
+
+# Create to make sure that at least 1 version property was specified
 if [ -z $VERSION_PROPERTIES ]
 then
   echo "version properties not specified.  Exiting"
   exit 1
 fi
 
-# Specify component name
-if [ -z $FILES_COMPONENTNAME ]
-then
-  echo "Component name not specified.  Exiting"
-  exit 1
-else
-  base_cmd+=" -component \"$FILES_COMPONENTNAME\""
-fi
-
-# Specify component version name
-if [ -z "$FILES_VERSIONNAME" ]
-then
-  echo "Version name not specified.  Exiting"
-  exit 1
-else
-  base_cmd+=" -version \"$FILES_VERSIONNAME\""
-fi
-
-# Specify local path
-if [ -z $FILES_BASE ]
-then
-  echo "No base files specified. Exiting"
-  exit 1
-else
-  base_cmd+=" -base \"$FILES_BASE\""
-fi
-
-if [ ! -z "$FILES_OFFSET" ]
-then
-  base_cmd+=" -offset \"$FILES_OFFSET\""
-fi
-
-if [ ! -z $FILES_INCLUDE ]
-then
-  base_cmd+=" -include \"$FILES_INCLUDE\""
-fi
-
-if [ ! -z $FILES_EXCLUDE ]
-then
-  base_cmd+=" -exclude \"$FILES_EXCLUDE\""
-fi
-
-if [ ! -z $FILES_SAVEEXECUTEBITS ]
-then
-  base_cmd+=" -saveExecuteBits \"$FILES_SAVEEXECUTEBITS\""
-fi
-
 echo $base_cmd
 
 # Invoke the udclient to add version files to the component version
-eval $base_cmd
+# eval $base_cmd
